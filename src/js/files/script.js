be499349f916eb8a7db4a8c77e1613ddc91a8925
Artem L <vlossaa@gmail.com>
2023-11-15 14:09:48 +0700

    feat: "all)"

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,23 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js'
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js'
+// import { gsap } from 'gsap'
+// import ScrollTrigger from 'gsap/ScrollTrigger.js'
+
+// let mm = gsap.matchMedia(),
+//     breakPoint = 768
+
+// gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+// ScrollSmoother.create({
+//     wrapper: '.wrapper',
+//     content: '.wrapper__scroll',
+//     smooth: 1.5,
+//     effects: true,
+// })
+// if (ScrollTrigger.isTouch !== 1) {
+//     // let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+//     // if (!isSafari) {
+//     //
+//     // }
+// }
