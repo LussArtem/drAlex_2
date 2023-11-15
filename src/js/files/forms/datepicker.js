8d03f2531cd70e155b79ea12d0e020f26e7268de
Artem L <vlossaa@gmail.com>
2023-11-15 14:03:07 +0700

    feat: "translate"

--- src/js/files/forms/datepicker.js
+++ src/js/files/forms/datepicker.js
@@ -1,26 +1,24 @@
-/* Календар */
+/* Календарь */
 
-// Підключення функціоналу "Чертоги Фрілансера"
-// Підключення списку активних модулів
-import { flsModules } from "../modules.js";
+// Подключение функционала "Чертоги Фрилансера"
+// Подключение списка активных модулей
+import { flsModules } from '../modules.js'
 
 // Підключення модуля
-import datepicker from 'js-datepicker';
+import datepicker from 'js-datepicker'
 
 if (document.querySelector('[data-datepicker]')) {
-	const picker = datepicker('[data-datepicker]', {
-		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
-		customMonths: ["Січ", "Лют", "Берез", "Квіт", "Трав", "Черв", "Лип", "Серп", "Верес", "Жовт", "Листоп", "Груд"],
-		overlayButton: 'Застосувати',
-		overlayPlaceholder: 'Рік (4 цифри)',
-		startDay: 1,
-		formatter: (input, date, instance) => {
-			const value = date.toLocaleDateString()
-			input.value = value
-		},
-		onSelect: function (input, instance, date) {
-
-		}
-	});
-	flsModules.datepicker = picker;
+    const picker = datepicker('[data-datepicker]', {
+        customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        customMonths: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'],
+        overlayButton: 'Применить',
+        overlayPlaceholder: 'Год (4 цифры)',
+        startDay: 1,
+        formatter: (input, date, instance) => {
+            const value = date.toLocaleDateString()
+            input.value = value
+        },
+        onSelect: function (input, instance, date) {},
+    })
+    flsModules.datepicker = picker
 }
